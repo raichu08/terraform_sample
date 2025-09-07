f92da19055d5fe4256481fc88c9dca122dacdb3f
raichu08 <45707558+raichu08@users.noreply.github.com>
2025-09-07 23:59:40 +0530

    Create main.tf

---
+++ statefile_lock/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region = "us-east-2"
+    
+}
+
+resource "aws_s3_bucket" "s3bucketforsatefile" {
+    bucket = "myterragaganlock"
+    tags = {
+        Name = "My_S3_bucket"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "s3bucketversioning" {
+    bucket = aws_s3_bucket.s3bucketforsatefile.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-lock"
+  }
+}
